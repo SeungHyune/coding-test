e4f123349b1e523bb19ebc03c3b79658a5d43273
seunghyun <tmdgus7820@naver.com>
2025-02-12 23:39:18 +0900

    solved: 제곱수의 합 / 2025-02-12

---
+++ baekjun/silver/제곱수의 합/제곱수의 합.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+let N = Number(input);
+const dp = [];
+for (let i = 0; i <= N; i++) dp[i] = i;
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j <= Math.sqrt(i); j++) {
+    dp[i] = Math.min(dp[i], dp[i - j ** 2] + 1);
+  }
+}
+console.log(dp[N]);
